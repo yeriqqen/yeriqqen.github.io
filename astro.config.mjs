37da60f4a3641dafa417b4238cd4707520c626f9
Yestay <yestayyessenbay@gmail.com>
2025-06-06 22:47:12 +0900

    fix: Update social links and export base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yeriqqen.github.io',
-  base: '/resume',
+  base: '/portfolio',
   vite: {
     plugins: [tailwindcss()]
   },
